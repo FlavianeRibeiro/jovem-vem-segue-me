7075a02b799e9dbeaf672796715e4577afd659b4
Flaviane Ribeiro <rnany13@gmail.com>
2016-10-21 22:40:30 +0000

    listagem por idade

--- pages/listagemPorIdade.php
+++ pages/listagemPorIdade.php
@@ -30,25 +30,6 @@
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             <i class="fa fa-bar-chart-o fa-fw"></i> Listagem por Idade
-                            <div class="pull-right">
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown">
-                                        Actions
-                                        <span class="caret"></span>
-                                    </button>
-                                    <ul class="dropdown-menu pull-right" role="menu">
-                                        <li><a href="#">Action</a>
-                                        </li>
-                                        <li><a href="#">Another action</a>
-                                        </li>
-                                        <li><a href="#">Something else here</a>
-                                        </li>
-                                        <li class="divider"></li>
-                                        <li><a href="#">Separated link</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
                         </div>
 						<!-- /.panel-heading -->
 						<div class="panel-body">
@@ -99,9 +80,7 @@
 										</tbody>
 									</table>
 								<!-- /.table-responsive -->
-							
 								</div> <!-- /.row -->
-				
 								<!-- ********************************* listagem ****************************************-->
 							</div>
 							<!-- /.panel-body -->
@@ -111,9 +90,7 @@
 					<!-- /.col-lg-12 -->
 				</div>
 				<!-- ********************************* listagem ****************************************-->
-			</div>
-		</div>
-		<div class="col-lg-4">
+			<div class="col-lg-4">
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             <i class="fa fa-bell fa-fw"></i> Notifications Panel
@@ -121,57 +98,37 @@
                         <!-- /.panel-heading -->
                         <div class="panel-body">
                             <div class="list-group">
-                                <a href="#" class="list-group-item">
-                                    <i class="fa fa-comment fa-fw"></i> New Comment
-                                    <span class="pull-right text-muted small"><em>4 minutes ago</em>
-                                    </span>
-                                </a>
-                                <a href="#" class="list-group-item">
-                                    <i class="fa fa-twitter fa-fw"></i> 3 New Followers
-                                    <span class="pull-right text-muted small"><em>12 minutes ago</em>
-                                    </span>
-                                </a>
-                                <a href="#" class="list-group-item">
-                                    <i class="fa fa-envelope fa-fw"></i> Message Sent
-                                    <span class="pull-right text-muted small"><em>27 minutes ago</em>
-                                    </span>
-                                </a>
-                                <a href="#" class="list-group-item">
-                                    <i class="fa fa-tasks fa-fw"></i> New Task
-                                    <span class="pull-right text-muted small"><em>43 minutes ago</em>
-                                    </span>
-                                </a>
-                                <a href="#" class="list-group-item">
-                                    <i class="fa fa-upload fa-fw"></i> Server Rebooted
-                                    <span class="pull-right text-muted small"><em>11:32 AM</em>
-                                    </span>
-                                </a>
-                                <a href="#" class="list-group-item">
-                                    <i class="fa fa-bolt fa-fw"></i> Server Crashed!
-                                    <span class="pull-right text-muted small"><em>11:13 AM</em>
-                                    </span>
-                                </a>
-                                <a href="#" class="list-group-item">
-                                    <i class="fa fa-warning fa-fw"></i> Server Not Responding
-                                    <span class="pull-right text-muted small"><em>10:57 AM</em>
-                                    </span>
-                                </a>
-                                <a href="#" class="list-group-item">
-                                    <i class="fa fa-shopping-cart fa-fw"></i> New Order Placed
-                                    <span class="pull-right text-muted small"><em>9:49 AM</em>
-                                    </span>
-                                </a>
-                                <a href="#" class="list-group-item">
-                                    <i class="fa fa-money fa-fw"></i> Payment Received
-                                    <span class="pull-right text-muted small"><em>Yesterday</em>
-                                    </span>
-                                </a>
+                            	<?php
+                            		$contadora =0;
+                            		$menor=16;
+                            		$maior=29;
+                            		$i=0;
+                            		while($menor != $maior){ 
+                            			while($contadora<count($Idade)){
+	                            			if($menor == $Idade[$contadora]){
+	                            				echo'<a href="#" class="list-group-item">'.$Idade[$contadora].' anos
+				                                    <span class="pull-right text-muted small"><em>'.$i++.'</em></span>
+				                                </a>';	
+	                            			}	
+										$contadora++;
+										$menor = $menor+1;
+										}
+                            		}
+                            	?>
                             </div>
                             <!-- /.list-group -->
-                            <a href="#" class="btn btn-default btn-block">View All Alerts</a>
                         </div>
                         <!-- /.panel-body -->
                     </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-4 -->
+            </div>
+            <!-- /.row -->
+			</div>
+			
+		</div>
+		
 	</div>
 </body>
 </html>
